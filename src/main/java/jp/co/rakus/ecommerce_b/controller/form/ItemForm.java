1bf1cbf6743cfd244e994eb782c13d231c2548de
yahiro <akihiko.yahiro@rakus.co.jp>
2017-09-04 09:06:38 +0900

    itemDomainとitemFromを作成しました。

---
+++ src/main/java/jp/co/rakus/ecommerce_b/controller/form/ItemForm.java
@@ -0,0 +1,66 @@
+package jp.co.rakus.ecommerce_b.controller.form;
+
+/**
+ * @author akihiko.yahiro (Item)商品フォーム
+ */
+public class ItemForm {
+
+	/** 商品id */
+	private String id;
+	/** 商品名 */
+	private String name;
+	/** 商品説明 */
+	private String description;
+	/** 商品M価格 */
+	private String priceM;
+	/** 商品L価格 */
+	private String priceL;
+	/** 商品の画像 */
+	private String imagePath;
+	/** 削除フラグ */
+	private String deleted;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPriceM() {
+		return priceM;
+	}
+	public void setPriceM(String priceM) {
+		this.priceM = priceM;
+	}
+	public String getPriceL() {
+		return priceL;
+	}
+	public void setPriceL(String priceL) {
+		this.priceL = priceL;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public String getDeleted() {
+		return deleted;
+	}
+	public void setDeleted(String deleted) {
+		this.deleted = deleted;
+	}
+
+}
---
+++ src/main/java/jp/co/rakus/ecommerce_b/domain/Item.java
@@ -0,0 +1,79 @@
+package jp.co.rakus.ecommerce_b.domain;
+
+/**
+ * @author akihiko.yahiro (Item)商品ドメイン
+ */
+public class Item {
+
+	/** 商品id */
+	private Integer id;
+	/** 商品名 */
+	private String name;
+	/** 商品説明 */
+	private String description;
+	/** 商品M価格 */
+	private Integer priceM;
+	/** 商品L価格 */
+	private Integer priceL;
+	/** 商品の画像 */
+	private String imagePath;
+	/** 削除フラグ */
+	private Boolean deleted;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPriceM() {
+		return priceM;
+	}
+
+	public void setPriceM(Integer priceM) {
+		this.priceM = priceM;
+	}
+
+	public Integer getPriceL() {
+		return priceL;
+	}
+
+	public void setPriceL(Integer priceL) {
+		this.priceL = priceL;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+}
