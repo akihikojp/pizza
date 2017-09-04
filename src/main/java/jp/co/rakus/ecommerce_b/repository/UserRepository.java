223f68b7df08a042e62956b75ab7260a0740fcb9
yutahonma <yuta.honma@rakus.co.jp>
2017-09-05 08:45:14 +0900

    userRepositoryを修正

--- src/main/java/jp/co/rakus/ecommerce_b/repository/UserRepository.java
+++ src/main/java/jp/co/rakus/ecommerce_b/repository/UserRepository.java
@@ -1,28 +1,30 @@
-//package jp.co.rakus.ecommerce_b.repository;
-//
-//import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-//import org.springframework.jdbc.core.RowMapper;
-//import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-//import org.springframework.stereotype.Repository;
-//
-///**
-// * @author yahiro
-// *ユーザー情報のレポジトリ
-// */
-//@Repository
-//public class UserRepository {
-//
-//	NamedParameterJdbcTemplate template;
-//	
-//	
-//	
-//	public static final RowMapper<User> userRowMapper = (rs,i) -> {
-//		User user = new User();
-//		user.setId(rs.getInt("id"));
-//		user.setName(rs.getString("name"));
-//		user.setEmail(rs.getString("email"));
-//		user.setPassword(rs.getString("password"));
-//		user.setTelephone(rs.getString("telephone"));
-//		return user;
-//	};
-//}
+package jp.co.rakus.ecommerce_b.repository;
+
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import jp.co.rakus.ecommerce_b.domain.User;
+
+/**
+ * @author yahiro
+ *ユーザー情報のレポジトリ
+ */
+@Repository
+public class UserRepository {
+
+	NamedParameterJdbcTemplate template;
+	
+	
+	
+	public static final RowMapper<User> userRowMapper = (rs,i) -> {
+		User user = new User();
+		user.setId(rs.getInt("id"));
+		user.setName(rs.getString("name"));
+		user.setEmail(rs.getString("email"));
+		user.setPassword(rs.getString("password"));
+		user.setTelephone(rs.getString("telephone"));
+		return user;
+	};
+}
